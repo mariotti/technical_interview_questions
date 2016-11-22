ea9f8ccd2687c94f00803fc94d580bf461cb21cc
Fabio <fab.mariotti@gmail.com>
2016-11-22 23:54:40 +0100

    shell: moved on awk for portability, make pretty printing and maybe more realiable times

--- shell_solutions/MostFrequentInt.sh
+++ shell_solutions/MostFrequentInt.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
 #
-./RandIntGen.sh | sort -n | uniq -c | sort -n | tail -1
+./RandIntGen.sh | tee x.$$.data | awk '{printf("%s ",$1)} END {print}'
+time cat x.$$.data | sort -n | uniq -c | sort -n | tail -3
+rm -rf x.$$.data
 #
--- shell_solutions/RandIntGen.sh
+++ shell_solutions/RandIntGen.sh
@@ -3,10 +3,13 @@
 # From: http://unix.stackexchange.com/questions/140750/generate-random-numbers-in-specific-range
 #
 # We just get one as example..
-# awk -v min=5 -v max=10 'BEGIN{srand(); print int(min+rand()*(max-min+1))}'
+awk -v min=0 -v max=9 -v rep=10 'BEGIN{srand(); ncount=0; while (ncount < rep) {ncount++; print int(min+rand()*(max-min+1))}}'
+exit 0
+# On mac example
 jot -r 1000 0 10
 exit 0
 #
+# One more example
 MACOS=1
 if [ $MACOS == 1 ]; then
     gshuf -i 1-100 -n 10;
