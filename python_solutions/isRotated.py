64862850c4e49068911240b32860c4030a914d1a
Fabio <fab.mariotti@gmail.com>
2016-11-29 21:59:25 +0100

    updates to bugs in isRotated

--- python_solutions/isRotated.py
+++ python_solutions/isRotated.py
@@ -3,6 +3,12 @@
 listA = [3, 4, 5, 6, 7, 8, 9, 0, 1, 2]
 listB = [7, 8, 9, 0, 1, 2, 3, 4, 5, 6]
 #
+A = [1,2,3,5,6,7,8]
+B = [5,6,7,8,1,2,3]
+
+C = [1,2,3]
+D = [6,7,5,8,1,2,3]
+#
 def isRotated(la,lb):
     # This function will not work if duplicates are alowed
     # But can be used in principle also in this case
@@ -14,17 +20,17 @@ def isRotated(la,lb):
         if (len(la) != len(lb)):
             return False
         print spos
-        print listA[spos:]
-        print listB[0:len(la)-spos]
-        print listA[:spos]
-        print listB[len(la)-spos:]
+        print la[spos:]
+        print lb[0:len(la)-spos]
+        print la[:spos]
+        print lb[len(la)-spos:]
 
-        if (listA[spos:] == listB[0:len(la)-spos]):
+        if (la[spos:] == lb[0:len(la)-spos]):
             print "First part true"
-        if (listA[:spos] == listB[len(la)-spos:]):
+        if (la[:spos] == lb[len(la)-spos:]):
             print "Second part true"
 
-    if (listA[spos:] == listB[0:len(la)-spos] and listA[:spos] == listB[len(la)-spos:]):
+    if (la[spos:] == lb[0:len(la)-spos] and la[:spos] == lb[len(la)-spos:]):
         return True
     else:
         return False
@@ -34,3 +40,6 @@ if __name__ == "__main__":
     print "B list:", listB
 
     print isRotated(listA, listB)
+    print isRotated(A, B)
+    print isRotated(B, C)
+    print isRotated(B, D)
