f19b77dc0b7a279b75e02bc281bdc951a6a40126
Fabio <fab.mariotti@gmail.com>
2017-03-31 00:58:20 +0200

    start using jq to generate markdown file

---
+++ bin/generateQmd.jq.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+#
+# JQ approach to generate an md (markdown) file
+echo "# Technical Questions and Algorithms\n"
+echo "## List of Categories\n"
+./bin/getCategories.sh | sed 's/^"/ - /' | sed 's/"$//'
+echo ""
+#
+# JQ Alternative: testing
+cat QUESTIONS.json | jq '.' | jq -r \
+    '.TechQuestions.category[] |
+    ( (.catname | "\n#Category: \(.)"),
+      (.question[] | ( 
+        (.name  | "\n## \(.)" ) , 
+        (.ID    | "\n    ID: \(.)"),
+        (.idCQ  | "    idCQ: \(.)"),
+        (.idQ   | "    idQ: \(.)"),
+        (.idC   | "    idC: \(.)"),
+        (.notes | "\n notes: \(.)")
+        )
+      )
+    )'
