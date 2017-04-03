5214690ab3483d157fb8306fce3c7ce375317dc5
Fabio <fab.mariotti@gmail.com>
2017-04-03 23:37:45 +0200

    random minor changes

--- python_solutions/Q1_MostFrequentInt.py
+++ python_solutions/Q1_MostFrequentInt.py
@@ -5,6 +5,7 @@ import random
 lista = [0, 1, 3, 2, 23, 43, 2, 1, 5, 2, 5, 2, 56, 23, 0, -1, 2, 2]
 listb = [4, 0, 1, 0, 4, 4, 4, 4, 0, 1, 1, 1,-6, 7]
 listc = [3, 5, 10, 3, 5, 1, 5, 3, 2, 3, 8, 4, 5, 1, 2, 7, 4, 6, 5, 8, 1, 1, 7, 10, 4, 5, 10, 5, 9, 1, 2, 10, 3, 9, 5, 10, 6, 10, 10, 8, 7, 2, 10, 7, 8, 10, 10, 7, 5, 10]
+
 def mostFrequent(nums):
     freq = {}
     for i in nums:
@@ -55,9 +56,11 @@ def mostFrequentV02(nums):
 if __name__ == "__main__":
 
     dostress = False
+    print "DATA"
     print "A list:", lista
     print "B list:", listb
     print "C list:", listc
+    print "RESULT"
     print "Most Frequent for a V0: " +  str(mostFrequent(lista))
     print "Most Frequent for b V0: " +  str(mostFrequent(listb))
     print "Most Frequent for c V0: " +  str(mostFrequent(listc))
@@ -65,14 +68,11 @@ if __name__ == "__main__":
     print "Most Frequent for b V1: " +  str(mostFrequentV02(listb))
     print "Most Frequent for c V1: " +  str(mostFrequentV02(listc))
 
-    print "C list Sorted:", listc
-
     listr = []
     if (dostress):
-        listr = my_randoms=[random.randrange(1,11,1) for _ in range (50000000)] #takes few minutes
+        listr = my_randoms=[random.randrange(1,11,1) for _ in range (5000000)] #takes few minutes
     else:
         listr = my_randoms=[random.randrange(1,11,1) for _ in range (500)]
-        print "R list:", listr
         
     print "Most Frequent for r V0: " +  str(mostFrequent(listr))
     print "Most Frequent for r V1: " +  str(mostFrequentV02(listr))
